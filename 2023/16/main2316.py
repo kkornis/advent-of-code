151101df5cd9057d68074f41095281d7b9afefa4
Kornis Kristof <kokract@gmail.com>
2023-12-16 11:12:37 +0100

    23 problem 16, first part solved

--- 2023/16/main2316.py
+++ 2023/16/main2316.py
@@ -1,3 +1,76 @@
+def is_place(subject: tuple[int, int, int]) -> bool:
+    return 0 <= subject[1] < 110 and 0 <= subject[2] < 110
+
+
+def determine_new_directions(direction: int, param: str) -> list[int]:
+    if param == '.':
+        return [direction]
+    elif param == '\\':
+        if direction == 0:
+            return [2]
+        elif direction == 1:
+            return [3]
+        elif direction == 2:
+            return [0]
+        elif direction == 3:
+            return [1]
+        else:
+            raise ValueError
+    elif param == '/':
+        if direction == 0:
+            return [3]
+        elif direction == 1:
+            return [2]
+        elif direction == 2:
+            return [1]
+        elif direction == 3:
+            return [0]
+        else:
+            raise ValueError
+    elif param == '-':
+        if direction == 0:
+            return [0]
+        elif direction == 1:
+            return [1]
+        elif direction == 2:
+            return [0, 1]
+        elif direction == 3:
+            return [0, 1]
+        else:
+            raise ValueError
+    elif param == '|':
+        if direction == 0:
+            return [2, 3]
+        elif direction == 1:
+            return [2, 3]
+        elif direction == 2:
+            return [2]
+        elif direction == 3:
+            return [3]
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+
+
+def apply_beam_raw(subject: tuple[int, int, int], param: str) -> list[tuple[int, int, int]]:
+    new_directions = determine_new_directions(subject[0], param)
+    new_subjects = []
+    for new_direction in new_directions:
+        if new_direction == 0:
+            new_subjects.append((new_direction, subject[1], subject[2] + 1))
+        elif new_direction == 1:
+            new_subjects.append((new_direction, subject[1], subject[2] - 1))
+        elif new_direction == 2:
+            new_subjects.append((new_direction, subject[1] + 1, subject[2]))
+        elif new_direction == 3:
+            new_subjects.append((new_direction, subject[1] - 1, subject[2]))
+    return new_subjects
+
+
+def apply_beam(subject: tuple[int, int, int], param: str) -> list[tuple[int, int, int]]:
+    return [x for x in apply_beam_raw(subject, param) if is_place(x)]
+
 
 def main():
     with open('input.txt') as inputtxt:
@@ -10,34 +83,32 @@ def main():
         width = len(lines[0])
         height = len(lines)
 
-        from_left = [[False] * width for i in range(height)]
-        from_right = [[False] * width for i in range(height)]
-        from_top = [[False] * width for i in range(height)]
-        from_bottom = [[False] * width for i in range(height)]
+        # from-left, -right, -top, -bottom
+        # from_dir x height x width
+        # data = [[[False] * width for i in range(height)] for j in range(4)]
+        #
+        # data[0][0][0] = True
+        final_data = [[False] * width for i in range(height)]
 
-        from_left[0][0] = True
-
-        unhandled = [(0, 0)]
+        unhandled = [(0, 0, 0)]
         start = 0
         end = 1
 
+        f = 0
         while end - start > 0:
-            x, y = unhandled[0]
-            if from_left[x][y]:
-                if lines[x][y] == '.':
-                    if y + 1 < width:
-                        if not from_left[x][y + 1]:
-                            from_left[x][y + 1] = True
-                            unhandled.append((x, y + 1))
-                            end += 1
-                elif lines[x][y] == '\\':
-                    pass
-                elif lines[x][y] == '/':
-                    pass
-                elif lines[x][y] == '-':
-                    pass
-                elif lines[x][y] == '|':
-                    pass
+            f += 1
+            print(f, start, end)
+            subject = unhandled[start]
+            new_subjects = apply_beam(subject, lines[subject[1]][subject[2]])
+            new_subjects = [x for x in new_subjects if x not in unhandled]
+            unhandled.extend(new_subjects)
+            end += len(new_subjects)
+            start += 1
+
+        for subject in unhandled:
+            final_data[subject[1]][subject[2]] = True
+
+        sum_a = sum([sum(x) for x in final_data])
 
         print(sum_a)
 
