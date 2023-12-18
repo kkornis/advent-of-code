e09d4946f3d4953f906e0decc5f8506cf2feb9ea
Kornis Kristof <kokract@gmail.com>
2023-12-18 22:18:47 +0100

    23 problem 17 second part.

--- 2023/17/main2317.py
+++ 2023/17/main2317.py
@@ -1,10 +1,15 @@
 def get_new_candidates(lines, vertex, val, height, width):
-    min_m = 1
-    max_m = 4
+    min_m = 4
+    max_m = 11
     candidates = []
     s0p = int(lines[vertex[1]][vertex[2]])
     s0n = int(lines[vertex[1]][vertex[2]])
     if vertex[0] == 0:
+        for i in range(1, min_m):
+            if vertex[1] + i < height:
+                s0p += int(lines[vertex[1] + i][vertex[2]])
+            if vertex[1] - i >= 0:
+                s0n += int(lines[vertex[1] - i][vertex[2]])
         for i in range(min_m, max_m):
             if vertex[1] + i < height:
                 candidates.append((1, vertex[1] + i, vertex[2], val + s0p))
@@ -13,6 +18,11 @@ def get_new_candidates(lines, vertex, val, height, width):
                 candidates.append((1, vertex[1] - i, vertex[2], val + s0n))
                 s0n += int(lines[vertex[1] - i][vertex[2]])
     elif vertex[0] == 1:
+        for i in range(1, min_m):
+            if vertex[2] + i < width:
+                s0p += int(lines[vertex[1]][vertex[2] + i])
+            if vertex[2] - i >= 0:
+                s0n += int(lines[vertex[1]][vertex[2] - i])
         for i in range(min_m, max_m):
             if vertex[2] + i < width:
                 candidates.append((0, vertex[1], vertex[2] + i, val + s0p))
@@ -26,9 +36,6 @@ def get_new_candidates(lines, vertex, val, height, width):
 def main():
     with open('input.txt') as inputtxt:
 
-        sum_a = 0
-        sum_b = 0
-
         lines = inputtxt.readlines()
         lines = [x[:-1] for x in lines]
         width = len(lines[0])
@@ -40,15 +47,15 @@ def main():
         candidates: dict[tuple[int, int, int], int] = {}
         reached[(0, height - 1, width - 1)] = 0
         reached[(1, height - 1, width - 1)] = 0
-        raw_candidates = get_new_candidates(lines, (0, height - 1, width - 1), 0, height, width) \
-                         + get_new_candidates(lines, (1, height - 1, width - 1), 0, height, width)
+        raw_candidates = get_new_candidates(lines, (0, height - 1, width - 1), 0, height, width) +\
+            get_new_candidates(lines, (1, height - 1, width - 1), 0, height, width)
         for raw_candidate in raw_candidates:
             candidates[(raw_candidate[0], raw_candidate[1], raw_candidate[2])] = raw_candidate[3]
         t = 0
         while len(candidates) > 0 and ((0, 0, 0) not in reached or (1, 0, 0) not in reached):
             t += 1
-            if t % 20 == 0:
-                print(t, len(reached))
+            if t % 10000 == 0:
+                print(t)
 
             minn = 10 ** 10
             key = None
