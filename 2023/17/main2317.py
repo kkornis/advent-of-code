a0aad3f6ebd5d4043bfaad0c00954e97859e3acd
Kornis Kristof <kokract@gmail.com>
2023-12-20 20:00:56 +0100

    23 problem 17 organize code.

--- 2023/17/main2317.py
+++ 2023/17/main2317.py
@@ -1,6 +1,4 @@
-def get_new_candidates(lines, vertex, val, height, width):
-    min_m = 4
-    max_m = 11
+def get_new_candidates(lines, vertex, val, height, width, min_m, max_m):
     candidates = []
     s0p = int(lines[vertex[1]][vertex[2]])
     s0n = int(lines[vertex[1]][vertex[2]])
@@ -33,30 +31,24 @@ def get_new_candidates(lines, vertex, val, height, width):
     return candidates
 
 
-def main():
+def main(min_m, max_m):
     with open('input.txt') as inputtxt:
-
         lines = inputtxt.readlines()
         lines = [x[:-1] for x in lines]
         width = len(lines[0])
         height = len(lines)
 
         # 0: horizontal, 1: vertical movement | row ind | col ind
-
         reached: dict[tuple[int, int, int], int] = {}
         candidates: dict[tuple[int, int, int], int] = {}
         reached[(0, height - 1, width - 1)] = 0
         reached[(1, height - 1, width - 1)] = 0
-        raw_candidates = get_new_candidates(lines, (0, height - 1, width - 1), 0, height, width) +\
-            get_new_candidates(lines, (1, height - 1, width - 1), 0, height, width)
+        raw_candidates = get_new_candidates(lines, (0, height - 1, width - 1), 0, height, width, min_m, max_m) +\
+            get_new_candidates(lines, (1, height - 1, width - 1), 0, height, width, min_m, max_m)
         for raw_candidate in raw_candidates:
             candidates[(raw_candidate[0], raw_candidate[1], raw_candidate[2])] = raw_candidate[3]
-        t = 0
-        while len(candidates) > 0 and ((0, 0, 0) not in reached or (1, 0, 0) not in reached):
-            t += 1
-            if t % 10000 == 0:
-                print(t)
 
+        while len(candidates) > 0 and ((0, 0, 0) not in reached or (1, 0, 0) not in reached):
             minn = 10 ** 10
             key = None
             for cand, value in candidates.items():
@@ -66,7 +58,7 @@ def main():
             assert key not in reached
             reached[key] = minn
             del candidates[key]
-            new_candidates = get_new_candidates(lines, key, reached[key], height, width)
+            new_candidates = get_new_candidates(lines, key, reached[key], height, width, min_m, max_m)
 
             for candidate in new_candidates:
                 key = (candidate[0], candidate[1], candidate[2])
@@ -78,10 +70,9 @@ def main():
                 else:
                     candidates[key] = candidate[3]
 
-        print(reached[(0, 0, 0)])
-        print(reached[(1, 0, 0)])
         print(min(reached[(0, 0, 0)], reached[(1, 0, 0)]))
 
 
 if __name__ == "__main__":
-    main()
+    main(1, 4)
+    main(4, 11)
