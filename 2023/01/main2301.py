3ad6c37f0a34971a84f4c6a92624b9d1560c49dd
Kornis Kristof <kokract@gmail.com>
2023-12-30 08:29:19 +0100

    23 problem 1 refact.

--- 2023/01/main2301.py
+++ 2023/01/main2301.py
@@ -1,34 +1,33 @@
-digits = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-str_digits = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-digits.update(str_digits)
 
-
-def find_sp_forward(line: str) -> str:
+def find_special(line: str, is_backward: bool, digits: dict) -> str:
     len_line = len(line)
     for i in range(len_line):
         for dig_str, dig_int in digits.items():
-            if line[i:].startswith(dig_str):
-                return str(dig_int)
+            if is_backward:
+                if line[:-i].endswith(dig_str):
+                    return str(dig_int)
+            else:
+                if line[i:].startswith(dig_str):
+                    return str(dig_int)
     raise IndexError
 
 
-def find_sp_backward(line: str) -> str:
-    len_line = len(line)
-    for i in range(len_line):
-        for dig_str, dig_int in digits.items():
-            if line[:-i].endswith(dig_str):
-                return str(dig_int)
-    raise IndexError
+def calculate_with_digits(lines, digits):
+    sum_scores = 0
+    for i, line in enumerate(lines):
+        code = int(find_special(line, False, digits) + find_special(line, True, digits))
+        sum_scores += code
+    return sum_scores
 
 
 def main():
     with open('input.txt') as inputtxt:
         lines = inputtxt.readlines()
-        sum_scores = 0
-        for i, line in enumerate(lines):
-            code = int(find_sp_forward(line) + find_sp_backward(line))
-            sum_scores += code
-        print(sum_scores)
+        digits = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        print('part a:', calculate_with_digits(lines, digits))
+        digits.update({'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+                       'nine': 9})
+        print('part b:', calculate_with_digits(lines, digits))
 
 
 if __name__ == "__main__":
