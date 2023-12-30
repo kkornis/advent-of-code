ee74f018c0c22f796e2714518db755370045b101
Kornis Kristof <kokract@gmail.com>
2023-12-30 10:53:07 +0100

    23 problem 8 refact.

--- 2023/08/main2308.py
+++ 2023/08/main2308.py
@@ -1,32 +1,36 @@
 
+def gcd(a, b):
+    if a < b:
+        return gcd(b, a)
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a - int(a / b) * b)
 
-def main():
+
+def main(part_b: bool):
     with open('input.txt') as inputtxt:
 
         lines = inputtxt.readlines()
         mmap = {}
 
+        instructions = lines[0][:-1]
+
         for line in lines[2:]:
             mmap[line[:3]] = (line[7:10], line[12:15])
 
-        startlist = [x for x in mmap.keys() if x.endswith('A')]
-        uniq = ['CKF', 'BMB', 'PSJ', 'VDN', 'NBB', 'FFB']
+        start_list = ['AAA']
+        if part_b:
+            start_list = [x for x in mmap.keys() if x.endswith('A')]
 
-        map_to_uniq = {x: y for x, y in zip(startlist, uniq)}
+        periods = {}
 
-        i = 0
-        good = False
-        state = 'AAA'
-
-        for state in startlist:
-            nzs = 0
+        for state in start_list:
             i = 0
             good = False
-            origstat = state
-            visited = set()
-            visited.add(state)
+            orig_stat = state
             while not good:
-                for j, char in enumerate(lines[0][:-1]):
+                for j, char in enumerate(instructions):
                     if not good:
                         if char == 'R':
                             state = mmap[state][1]
@@ -35,18 +39,17 @@ def main():
                             state = mmap[state][0]
                             i = i + 1
                         if state.endswith('Z'):
-                            # print(state)
-                            print('i: ' + str(i))
-                            # print(char)
-                            nzs += 1
-                if i > 100000:
-                    good = True
-                    # print(state)
-                    # print(nzs)
-                else:
-                    visited.add(state)
-            print(i)
+                            assert j == len(instructions) - 1
+                            periods[orig_stat] = i
+                            good = True
+
+        res = 1
+        for period in periods.values():
+            res = res * int(period / gcd(res, period))
+
+        return res
 
 
 if __name__ == "__main__":
-    main()
+    print('part a:', main(False))
+    print('part b:', main(True))
