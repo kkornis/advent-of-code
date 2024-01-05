78f59d2408fbe711ed2b5cde25fa093aec7330da
Kornis Kristof <kokract@gmail.com>
2024-01-05 18:31:34 +0100

    23 problem 12 start indexing from the other end.

--- 2023/12/main2312.py
+++ 2023/12/main2312.py
@@ -16,29 +16,29 @@ def main(part_b):
 
             solution_table = [[]]
             for j in range(len(code) + 1):
-                if '#' in code[len_code - j:]:
+                if '#' in code[:j]:
                     solution_table[0].append(0)
                 else:
                     solution_table[0].append(1)
 
-            for i in range(1, len(numbers) + 1):
+            for i in range(len(numbers)):
                 solution_table.append([0])
-                for j in range(1, len(code) + 1):
-                    must_start_with = code[len_code - j] == '#'
+                for j in range(len_code):
+                    must_ends_with = code[j] == '#'
 
-                    fit_start = j >= numbers[len(numbers) - i]
-                    fit_start = fit_start and ('.' not in code[len_code - j: len_code - j + numbers[len(numbers) - i]])
-                    if len_code > len_code - j + numbers[len(numbers) - i]:
-                        fit_start = fit_start and code[len_code - j + numbers[len(numbers) - i]] != '#'
+                    fit_end = j + 1 >= numbers[i]
+                    fit_end = fit_end and ('.' not in code[j + 1 - numbers[i]:j + 1])
+                    if j + 1 > numbers[i]:
+                        fit_end = fit_end and code[j - numbers[i]] != '#'
 
                     local_res = 0
-                    if fit_start:
-                        l_ind = max(0, j - numbers[len(numbers) - i] - 1)
-                        local_res += solution_table[i - 1][l_ind]
-                    if not must_start_with:
-                        local_res += solution_table[i][j - 1]
+                    if fit_end:
+                        l_ind = max(0, j + 1 - numbers[i] - 1)
+                        local_res += solution_table[i][l_ind]
+                    if not must_ends_with:
+                        local_res += solution_table[i + 1][j]
 
-                    solution_table[i].append(local_res)
+                    solution_table[i + 1].append(local_res)
 
             sum_a += solution_table[len(numbers)][len(code)]
         return sum_a
