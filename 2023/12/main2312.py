abab83ad8694c2f47638b8e3344b64f0866405e6
Kornis Kristof <kokract@gmail.com>
2024-01-05 18:39:15 +0100

    23 problem 12 use enumerate, rename.

--- 2023/12/main2312.py
+++ 2023/12/main2312.py
@@ -1,4 +1,3 @@
-
 def main(part_b):
     with open('input.txt') as inputtxt:
 
@@ -7,40 +6,35 @@ def main(part_b):
 
         for l_num, line in enumerate(lines):
             code, numbers_part = line[:-1].split()
-            numbers = [int(x) for x in numbers_part.split(',')]
+            groups = [int(x) for x in numbers_part.split(',')]
             if part_b:
                 code = '?'.join([code] * 5)
-                numbers = numbers * 5
-
-            len_code = len(code)
+                groups = groups * 5
 
             solution_table = [[]]
             for j in range(len(code) + 1):
-                if '#' in code[:j]:
-                    solution_table[0].append(0)
-                else:
-                    solution_table[0].append(1)
+                solution_table[0].append(0 if '#' in code[:j] else 1)
 
-            for i in range(len(numbers)):
+            for i, group in enumerate(groups):
                 solution_table.append([0])
-                for j in range(len_code):
+                for j in range(len(code)):
                     must_ends_with = code[j] == '#'
 
-                    fit_end = j + 1 >= numbers[i]
-                    fit_end = fit_end and ('.' not in code[j + 1 - numbers[i]:j + 1])
-                    if j + 1 > numbers[i]:
-                        fit_end = fit_end and code[j - numbers[i]] != '#'
+                    fit_end = j + 1 >= group
+                    fit_end = fit_end and ('.' not in code[j + 1 - group:j + 1])
+                    if j + 1 > group:
+                        fit_end = fit_end and code[j - group] != '#'
 
                     local_res = 0
                     if fit_end:
-                        l_ind = max(0, j + 1 - numbers[i] - 1)
+                        l_ind = max(0, j + 1 - group - 1)
                         local_res += solution_table[i][l_ind]
                     if not must_ends_with:
                         local_res += solution_table[i + 1][j]
 
                     solution_table[i + 1].append(local_res)
 
-            sum_a += solution_table[len(numbers)][len(code)]
+            sum_a += solution_table[len(groups)][len(code)]
         return sum_a
 
 
