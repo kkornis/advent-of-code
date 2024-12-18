65038e7fc9a2221e8ddac7c8126b13c3a142c6f5
Kornis Kristof <kokract@gmail.com>
2024-12-18 11:44:24 +0100

    2418 simplify

--- 2024/18/main2418.py
+++ 2024/18/main2418.py
@@ -2,8 +2,8 @@ neighbours = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
 
 def solve_one(width, height, rel_bytes):
-    in_pat = {(0, 0): (None, 0)}
-    new_places = {(0, 0): (None, 0)}
+    in_pat = {(0, 0): 0}
+    new_places = {(0, 0): 0}
 
     while len(new_places) > 0 and (width - 1, height - 1) not in in_pat:
         new_new_places = {}
@@ -11,16 +11,15 @@ def solve_one(width, height, rel_bytes):
             for n_x, n_y in neighbours:
                 cand = new_place[0] + n_x, new_place[1] + n_y
                 if cand not in rel_bytes and 0 <= cand[0] < width and 0 <= cand[1] < height and cand not in in_pat:
-                    in_pat[cand] = (new_place, dist[1] + 1)
-                    new_new_places[cand] = (new_place, dist[1] + 1)
+                    in_pat[cand] = dist + 1
+                    new_new_places[cand] = dist + 1
         new_places = new_new_places
-    return in_pat[(width - 1, height - 1)][1] if (width - 1, height - 1) in in_pat else None
+    return in_pat[(width - 1, height - 1)] if (width - 1, height - 1) in in_pat else None
 
 
 def main():
     with open("input.txt") as inputtxt:
         f_bytes = [(int(line.split(',')[0]), int(line.split(',')[1])) for line in inputtxt.readlines()]
-
         height = 71
         width = 71
 
