c7be8f58ef9cbda77aadf7b75067b1dc7e484ecb
Kornis Kristof <kokract@gmail.com>
2024-12-18 11:39:58 +0100

    2418 part 2, simplify

--- 2024/18/main2418.py
+++ 2024/18/main2418.py
@@ -1,45 +1,38 @@
 neighbours = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
 
+def solve_one(width, height, rel_bytes):
+    in_pat = {(0, 0): (None, 0)}
+    new_places = {(0, 0): (None, 0)}
+
+    while len(new_places) > 0 and (width - 1, height - 1) not in in_pat:
+        new_new_places = {}
+        for new_place, dist in new_places.items():
+            for n_x, n_y in neighbours:
+                cand = new_place[0] + n_x, new_place[1] + n_y
+                if cand not in rel_bytes and 0 <= cand[0] < width and 0 <= cand[1] < height and cand not in in_pat:
+                    in_pat[cand] = (new_place, dist[1] + 1)
+                    new_new_places[cand] = (new_place, dist[1] + 1)
+        new_places = new_new_places
+    return in_pat[(width - 1, height - 1)][1] if (width - 1, height - 1) in in_pat else None
+
+
 def main():
     with open("input.txt") as inputtxt:
         f_bytes = [(int(line.split(',')[0]), int(line.split(',')[1])) for line in inputtxt.readlines()]
-        print(len(f_bytes))
-        for f_b in f_bytes:
-            assert 2 == len(f_b)
 
         height = 71
         width = 71
-        i = 1024
-        # go = True
-        # while go:
-        rel_bytes = f_bytes[:1024]
-        # height = 7
-        # width = 7
-        # rel_bytes = f_bytes[:12]
 
-        in_pat = {(0, 0): (None, 0)}
-        new_places = {(0, 0): (None, 0)}
+        print('Part One: ', solve_one(width, height, f_bytes[:1024]))
 
-        while len(new_places) > 0 and (70, 70) not in in_pat:
-            new_new_places = {}
-            for new_place, dist in new_places.items():
-                if new_place == (6, 0):
-                    print('x')
-                for n_x, n_y in neighbours:
-                    cand = new_place[0] + n_x, new_place[1] + n_y
-                    if cand not in rel_bytes and 0 <= cand[0] < width and 0 <= cand[1] < height and cand not in in_pat:
-                        in_pat[cand] = (new_place, dist[1] + 1)
-                        new_new_places[cand] = (new_place, dist[1] + 1)
-            new_places = new_new_places
+        i = 3020
+        dist = 0
+        while dist is not None:
+            dist = solve_one(width, height, f_bytes[:i])
+            i += 1
 
-        print('Part One: ', in_pat[(width - 1, height - 1)][1])
-
-        # x = (width - 1, height - 1)
-        # while x != (0, 0):
-        #     x, dist = in_pat[x]
-        #     print(x, dist)
-        # print('')
+        print('Part Two: ', f_bytes[i - 2])
 
 
 if __name__ == "__main__":
