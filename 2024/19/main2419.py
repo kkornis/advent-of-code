2bad2c3c49b91755e15a8f8bfaa6d0b142cd5497
Kornis Kristof <kokract@gmail.com>
2024-12-19 10:15:34 +0100

    2419

---
+++ 2024/19/main2419.py
@@ -0,0 +1,22 @@
+def constructable(towel: str, constituents: list[str]) -> bool:
+    n_constructions = [1]
+    for i in range(len(towel)):
+        n_constructions.append(0)
+        for constituent in constituents:
+            if towel[-i-1:].startswith(constituent):
+                n_constructions[-1] += n_constructions[-len(constituent)-1]
+    return n_constructions[-1]
+
+
+def main():
+    with open("input.txt") as inputtxt:
+        lines = inputtxt.readlines()
+        constituents = lines[0][:-1].split(', ')
+
+        n_constructions = [constructable(towel[:-1], constituents) for towel in lines[2:]]
+        print('Part One: ', sum([x > 0 for x in n_constructions]))
+        print('Part Two: ', sum(n_constructions))
+
+
+if __name__ == "__main__":
+    main()
